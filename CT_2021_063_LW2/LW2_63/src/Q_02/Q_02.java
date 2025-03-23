0d0b86d2ee778a2e8e41c0534ae6ece8ec65716c
KavinduNirwan123 <sakndilsara@gmail.com>
2025-03-23 14:52:15 +0530

    Add files via upload

---
+++ CT_2021_063_LW2/LW2_63/src/Q_02/Q_02.java
@@ -0,0 +1,12 @@
+package Q_02;
+import java.util.Scanner;
+public class Q_02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the value in inches : ");
+        double inches = scan.nextDouble();
+        double cm = inches * 2.54;
+        System.out.println(inches + " inches --> " + cm + " cm.");
+    }
+}
+
